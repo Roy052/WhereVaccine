a02cd21e6f5c7e547b55e8baccb29a0d708c218d
ideasidus <ideasidus@gmail.com>
2021-06-17 19:46:15 +0900

    지도보기 크기조절

--- WhereVaccine/MapView.swift
+++ WhereVaccine/MapView.swift
@@ -11,7 +11,7 @@ import MapKit
 struct MapView: View {
 
     @State private var locations: [Location] = Location.getLocation()
-    @State private var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 35.8889591, longitude: 128.6095541), span: MKCoordinateSpan(latitudeDelta: 0.3, longitudeDelta: 0.3))
+    @State private var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 35.8889591, longitude: 128.6095541), span: MKCoordinateSpan(latitudeDelta: 0.03, longitudeDelta: 0.03))
 
     var body: some View {
         Map(coordinateRegion: $region, annotationItems: locations, annotationContent:  { (location) -> MapPin in
